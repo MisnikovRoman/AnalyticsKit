73cfd5ec67c54f79f260d2d8a97fc4b015d83efb
Roman <misnikovroman@gmail.com>
2019-02-15 20:17:22 +0300

    Создал пустой проект библиотеки

---
+++ AnalyticsKit/AnalyticsKit.h
@@ -0,0 +1,19 @@
+//
+//  AnalyticsKit.h
+//  AnalyticsKit
+//
+//  Created by MisnikovRoman on 15/02/2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalyticsKit.
+FOUNDATION_EXPORT double AnalyticsKitVersionNumber;
+
+//! Project version string for AnalyticsKit.
+FOUNDATION_EXPORT const unsigned char AnalyticsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsKit/PublicHeader.h>
+
+
