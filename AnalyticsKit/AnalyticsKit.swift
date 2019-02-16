1a178546d045c4a668edcc9d27e4526fc4a5bcdf
Roman <misnikovroman@gmail.com>
2019-02-16 14:29:50 +0300

    Перенес .h файлы в одну папку с module.modulemap файлом и исправить Build Settings.  Удачно сбилдилось

---
+++ AnalyticsKit/AnalyticsKit.swift
@@ -0,0 +1,19 @@
+//
+//  AnalyticsKit.swift
+//  AnalyticsKit
+//
+//  Created by MisnikovRoman on 15/02/2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+import Foundation
+
+class AnalyticsKit {
+    private init () {}
+    static let shared = AnalyticsKit()
+    
+    func test() {
+        guard let gai = GAI.sharedInstance() else { return }
+        print("Analytics is working 🎉")
+    }
+}
